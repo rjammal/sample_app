deea46fc4d47b785b162c797125e89b05adb30c7
rjammal <rjammal@gmail.com>
2014-05-30 10:23:11 -0700

    finished chapter 8

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,4 @@
 class SessionsController < ApplicationController
-<<<<<<< HEAD
-=======
 
   def new
   end
@@ -8,7 +6,8 @@ class SessionsController < ApplicationController
   def create 
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      #sign the user in and redirect to the user's show page
+      sign_in user
+      redirect_to user
     else
       flash.now[:error] = "Invalid email/password combination"
       render 'new'
@@ -16,7 +15,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    sign_out
+    redirect_to root_url
   end
 
->>>>>>> sign-in-out
 end
